3d2fff227c4bb9a036ae5d3a12e8c1eb33ec36a5
Bruce Markham <219281+brucificus@users.noreply.github.com>
2024-12-16 13:17:28 -0500

    ⬆️Updates Dependencies

--- src/aliases.ps1
+++ src/aliases.ps1
@@ -1,6 +1,8 @@
 #!/usr/bin/env pwsh
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
+#Requires -Modules @{ ModuleName = "poshy-ecks"; RequiredVersion = "0.5.22" }
+#Requires -Modules @{ ModuleName = "poshy-lucidity"; RequiredVersion = "0.4.1" }
 
 
 if (-not (Test-Command git) -and (-not (Get-Variable -Name PWSHRC_FORCE_MODULES_EXPORT_UNSUPPORTED -Scope Global -ValueOnly -ErrorAction SilentlyContinue))) {
